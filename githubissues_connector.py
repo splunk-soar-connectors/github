598da78aa753712c68a1b0945ec8e70b65378c76
Jay Patel <jay.patel@M06-JayP.local>
2019-07-29 17:44:03 +0530

    GitHub Issues: Fixing the copyrights unicode extended dash issue

--- githubissues_connector.py
+++ githubissues_connector.py
@@ -1,7 +1,7 @@
 # File: githubissues_connector.py
 # Copyright (c) 2019 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
 
 # Phantom App imports
